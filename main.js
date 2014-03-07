a94eca5e3eac8f3b82d199871733a2f4ea265bea
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-03-07 18:46:33 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,13 @@
+define(function (require, exports, module) {
+  'use strict';
+  
+  var LanguageManager = brackets.getModule("language/LanguageManager");
+
+  LanguageManager.defineLanguage("rust", {
+      name: "Rust",
+      mode: "rust",
+      fileExtensions: ["rs"],
+      blockComment: ["/*", "*/"],
+      lineComment: ["//"]
+  });
+});
