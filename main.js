bd0cc703f3ba5a7b5af46fef0a49b58a3f7b09ef
Fredrik Forsmo <frozzare@users.noreply.github.com>
2014-03-08 13:05:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 define(function (require, exports, module) {
   'use strict';
   
-  var LanguageManager = brackets.getModule("language/LanguageManager");
+  var LanguageManager = brackets.getModule('language/LanguageManager');
 
-  LanguageManager.defineLanguage("rust", {
-      name: "Rust",
-      mode: "rust",
-      fileExtensions: ["rs"],
-      blockComment: ["/*", "*/"],
-      lineComment: ["//"]
+  LanguageManager.defineLanguage('rust', {
+      name: 'Rust',
+      mode: 'rust',
+      fileExtensions: ['rs'],
+      blockComment: ['/*', '*/'],
+      lineComment: ['//']
   });
 });
